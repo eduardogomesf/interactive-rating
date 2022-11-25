045c812e5e9337132e28337970d313cda799ee28
Eduardo Gomes <eduardogoomesf@gmail.com>
2022-11-25 15:41:55 -0300

    feat: [rating] add interactive rating to app

--- interactive-rating/src/App.tsx
+++ interactive-rating/src/App.tsx
@@ -1,10 +1,11 @@
+import { InteractiveRating } from "./components/InteractiveRating";
 import { GlobalContentContainer, GlobalStyles } from "./styles/global";
 
 export function App () {
   return (
     <>
       <GlobalContentContainer>
-        <h1>Hello world</h1>
+        <InteractiveRating />
       </GlobalContentContainer>
       <GlobalStyles />
     </>
